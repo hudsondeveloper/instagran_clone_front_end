a4aa03c055a0bfe6433b291e2d633381ec93f74a
hudsondeveloper <hudsonluis.costa@gmail.com>
2018-08-06 21:55:21 -0300

    procfile

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
-app.listen(8000, function(){
-	console.log('Servidor online na porta 8000');
-})
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
